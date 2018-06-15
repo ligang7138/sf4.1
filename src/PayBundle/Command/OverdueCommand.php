559509e4526bf43354270b36d913dfd0a826ad87
ligang7138 <445123875@qq.com>
2018-06-15 16:13:12 +0800

    bundle中增加console命令

---
+++ src/PayBundle/Command/OverdueCommand.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\PayBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * 逾期还款发送短信
+ * Class OverdueCommand
+ * @package AdminBundle\Command
+ */
+class OverdueCommand extends ContainerAwareCommand{
+
+	protected function configure(){
+		$this
+			->setName('overdue:send-message')
+			->setDescription('逾期还款发送短信');
+	}
+
+	protected function execute(InputInterface $input, OutputInterface $output){
+		echo 2323;
+	}
+}
